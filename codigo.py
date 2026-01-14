0290f1435e966a8ec3a554b984b2a68c7ddce122
andressa-leite <dre_tl@hotmail.com>
2026-01-14 14:06:37 -0300

    Add automation workflow steps for product registration

---
+++ codigo.py
@@ -0,0 +1,6 @@
+# step 1: access the website - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# step 2: login with your credentials
+# step 3: Import dadabase (products.csv)
+# step 4: register a product
+# step 5: repeat step 4 for all products in the database
+# pyautogui - library to automate mouse and keyboard actions
